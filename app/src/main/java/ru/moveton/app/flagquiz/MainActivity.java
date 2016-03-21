efac3455c8b34762da6adef8e8823a0db2cd5a2d
Maxim Gavrilenko <parrysh@mail.ru>
2016-03-21 22:08:07 +0300

    Coding MainFragment

--- app/src/main/java/ru/moveton/app/flagquiz/MainActivity.java
+++ app/src/main/java/ru/moveton/app/flagquiz/MainActivity.java
@@ -1,6 +1,11 @@
 package ru.moveton.app.flagquiz;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +13,44 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
+
+    private boolean phoneDevice = true;
+    private boolean preferencesChanged = true;
+    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangeListener =
+            new SharedPreferences.OnSharedPreferenceChangeListener() {
+                @Override
+                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                    preferencesChanged = true;
+
+                    MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+
+                    if (key.equals(CHOICES)) {
+                        quizFragment.updateGuessRows(sharedPreferences);
+                        quizFragment.resetQuiz();
+                    } else if (key.equals(REGIONS)) {
+                        Set<String> regions = sharedPreferences.getStringSet(REGIONS, null);
+                        if (regions != null && regions.size() > 0) {
+                            quizFragment.updateRegions(sharedPreferences);
+                            quizFragment.resetQuiz();
+                        } else {
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            regions.add(getString(R.string.default_region));
+                            editor.putStringSet(REGIONS, regions);
+                            editor.apply();
+                            Toast.makeText(MainActivity.this, R.string.default_region_message, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    Toast.makeText(MainActivity.this, R.string.restarting_quiz, Toast.LENGTH_SHORT).show();
+                }
+            };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +59,34 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(preferencesChangeListener);
+
+        int screenSize = getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+            phoneDevice = false;
+        }
+
+        if (phoneDevice) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        } else {
+            return false;
+        }
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
     }
 
     @Override
@@ -33,13 +94,22 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
+        return super.onOptionsItemSelected(item);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (preferencesChanged) {
+            MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+            quizFragment.updateGuessRows(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.updateRegions(PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.resetQiuz();
+            preferencesChanged = false;
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/ru/moveton/app/flagquiz/MainActivityFragment.java
+++ app/src/main/java/ru/moveton/app/flagquiz/MainActivityFragment.java
@@ -1,15 +1,56 @@
 package ru.moveton.app.flagquiz;
 
+import android.annotation.TargetApi;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private static final String TAG = "FlagQuiz Activity";
+
+    private static final int FLAGS_IN_QUIZ = 10;
+    private List<String> fileNameList;
+    private List<String> quizCountriesList;
+    private SecureRandom random;
+    private Handler handler;
+    private Animation shakeAnimation;
+    private LinearLayout quizLinearLayout;
+    private LinearLayout[] guessLinearLayouts;
+    private TextView questionNumberTextView;
+    private ImageView flagImageView;
+    private TextView answerTextView;
+    private int guessRows;
+    private Set<String> regionsSet;
+    private int correctAnswers;
+    private int totalGuesses;
+    private String correctAnswer;
+
 
     public MainActivityFragment() {
     }
@@ -17,6 +58,122 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        fileNameList = new ArrayList<>();
+        quizCountriesList = new ArrayList<>();
+        random = new SecureRandom();
+        handler = new Handler();
+        shakeAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.incorrect_shake);
+        shakeAnimation.setRepeatCount(3);
+
+        quizLinearLayout = (LinearLayout) view.findViewById(R.id.quizLinearLayout);
+        questionNumberTextView = (TextView) view.findViewById(R.id.questionNumberTextEdit);
+        flagImageView = (ImageView) view.findViewById(R.id.flagImageView);
+
+        guessLinearLayouts = new LinearLayout[4];
+        guessLinearLayouts[0] = (LinearLayout) view.findViewById(R.id.row1LinearLayout);
+        guessLinearLayouts[1] = (LinearLayout) view.findViewById(R.id.row2LinearLayout);
+        guessLinearLayouts[2] = (LinearLayout) view.findViewById(R.id.row3LinearLayout);
+        guessLinearLayouts[3] = (LinearLayout) view.findViewById(R.id.row4LinearLayout);
+        answerTextView = (TextView) view.findViewById(R.id.answerTextView);
+        for (LinearLayout row :
+                guessLinearLayouts) {
+            for (int column = 0; column < row.getChildCount(); column++) {
+                Button button = (Button) row.getChildAt(column);
+                button.setOnClickListener(guessButtonListener);
+            }
+        }
+
+        questionNumberTextView.setText(getString(R.string.question, 1, FLAGS_IN_QUIZ));
+        return view;
+    }
+
+
+    public void updateGuessRows(SharedPreferences sharedPreferences) {
+        String choices = sharedPreferences.getString(MainActivity.CHOICES, null);
+        guessRows = Integer.parseInt(choices) / 2;
+
+        for (LinearLayout layout :
+                guessLinearLayouts) {
+            layout.setVisibility(ViewGroup.GONE);
+        }
+
+        for (int row = 0; row < guessRows; row++) {
+            guessLinearLayouts[row].setVisibility(View.VISIBLE);
+        }
+    }
+
+
+    public void updateRegions(SharedPreferences sharedPreferences) {
+        regionsSet = sharedPreferences.getStringSet(MainActivity.REGIONS, null);
+    }
+
+    public void resetQuiz() {
+        AssetManager assets = getActivity().getAssets();
+        fileNameList.clear();
+
+        try {
+            for (String region :
+                    regionsSet) {
+                String[] paths = assets.list(region);
+                for (String path :
+                        paths) {
+                    fileNameList.add(path.replace(".png", ""));
+                }
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading image file names", e);
+        }
+
+        correctAnswers = 0;
+        totalGuesses = 0;
+        quizCountriesList.clear();
+
+        int flagCounter = 1;
+
+        int numberOfFlags = fileNameList.size();
+
+        while (flagCounter <= FLAGS_IN_QUIZ) {
+            int randomIndex = random.nextInt(numberOfFlags);
+            String filename = fileNameList.get(randomIndex);
+
+            if (!quizCountriesList.contains(filename)) {
+                quizCountriesList.add(filename);
+                ++flagCounter;
+            }
+        }
+        loadNextFlag();
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private void loadNextFlag() {
+        String nextImage = quizCountriesList.remove(0);
+        correctAnswer = nextImage;
+        answerTextView.setText("");
+
+        questionNumberTextView.setText(getString(R.string.question, (correctAnswers + 1), FLAGS_IN_QUIZ));
+
+        String region = nextImage.substring(0, nextImage.indexOf('-'));
+        AssetManager assets = getActivity().getAssets();
+
+        try (InputStream stream = assets.open(region + "/" + nextImage + ".png")) {
+            Drawable flag = Drawable.createFromStream(stream, nextImage);
+            flagImageView.setImageDrawable(flag);
+
+            animate(false);
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading" + nextImage, e);
+        }
+
+        Collections.shuffle(fileNameList);
+
+        int correct = fileNameList.indexOf(correctAnswer);
+        fileNameList.add(fileNameList.remove(correct));
+    }
+
+    private void animate(boolean b) {
+
     }
 }
